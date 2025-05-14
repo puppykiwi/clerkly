97023786c73ac4b1fed892010ad49b11cf960b30
puppykiwi <johnraymwendwa04@gmail.com>
2025-05-14 17:44:53 +0300

    cathing E's

--- UI/connectdb.php
+++ UI/connectdb.php
@@ -1,5 +1,11 @@
 <?php
-$pdo = new PDO('mysql:host:localhost;dbname:clerkly','clerkly','0700479408');
+
+try {
+    $pdo = new PDO('mysql:host:localhost;dbname:clerkly','clerkly','0700479408');
+} catch (PDOException $e) {
+
+echo $e->getMessage(); 
+}
 
 echo'connected';
 
